ded72f2889521cc112f433be7d17e3bcecfb23c1
owias azizi <owaisazizi360@gmail.com>
2025-01-06 16:34:05 +0430

    read and write file async and sync

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const { dirname } = require('path');
 const url = require('url');
 
 
+//Files
+//sycrounose(blocking)
+////read
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8')
+// console.log(textIn);
+
+// // write
+// const textOut = `this is what we create ${textIn} \n created ${Date.now()}`
+//  fs.writeFileSync('./txt/output.txt',textOut)
+
+//asyncronouse(non=blocking)
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//         console.log(data2+'****');
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//           console.log(`${data3}`);
+//           fs.writeFile('./txt/final.txt', `${data2} ------------ to ${data3}. `,'utf-8',err=>{
+//             console.log('wrting to the final finished👌👌');
+//           })
+//         })
+
+//     })
+// })
+// console.log('will read the file');
 
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8',);
