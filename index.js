66cfbfa8b2d08d8d027ab822a82632f43b4033bf
owias azizi <owaisazizi360@gmail.com>
2025-01-07 15:43:49 +0430

    some update and installations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replaceTemplate')
 
 //Files
@@ -48,16 +48,20 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8',);
 const dataObj = JSON.parse(data)
 
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+console.log(slugs);
+
+
 
 ////Server every thing in it recreate for each request
 const server = http.createServer((req, res) => {
     // const pathname = req.url
-    const {query,pathname} = url.parse(req.url,true)
+    const { query, pathname } = url.parse(req.url, true)
 
     //overviw
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' })
-        const cartHtml = dataObj.map(el=>replaceTemplate(tempCart,el)).join('')        
+        const cartHtml = dataObj.map(el => replaceTemplate(tempCart, el)).join('')
         const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cartHtml)
         res.end(output)
     }
@@ -66,7 +70,7 @@ const server = http.createServer((req, res) => {
     else if (pathname === '/product') {
         res.writeHead(200, { 'Content-type': 'text/html' })
         const product = dataObj[query.id]
-        const output = replaceTemplate(tempProduct,product)
+        const output = replaceTemplate(tempProduct, product)
         res.end(output)
     }
 
