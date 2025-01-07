106a454bdc3a43a29a59c3a027f0c4f5f1b5b83b
owias azizi <owaisazizi360@gmail.com>
2025-01-07 11:27:07 +0430

    change the overveiw template to original data

--- index.js
+++ index.js
@@ -28,11 +28,22 @@ const url = require('url');
 // console.log('will read the file');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8',);
-const tempCart = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8',);
+const tempCart = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8',);
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8',);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8',);
-const dataObject = JSON.parse(data)
+const dataObj = JSON.parse(data)
+
+const replaceTemplate = (tempCart,product)=>{
+    let output = tempCart.replace(/{%PRODUCTNAME%}/g, product.productName)
+     output = output.replace(/{%IMAGE%}/g, product.image)
+     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+     output = output.replace(/{%QUANTITY%}/g, product.quantity)
+     output = output.replace(/{%PRICE%}/g, product.price)
+     output = output.replace(/{%ID%}/g, product.id)
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')        
+    return output
+    }
 
 ////Server every thing in it recreate for each request
 const server = http.createServer((req, res) => {
@@ -40,14 +51,18 @@ const server = http.createServer((req, res) => {
 
     //overviw
     if (pathName === '/' || pathName === '/overview') {
-        // res.end('Hello from your servessssssssr')
         res.writeHead(200, { 'Content-type': 'text/html' })
-        res.end(tempOverview)
+
+        const cartHtml = dataObj.map(el=>replaceTemplate(tempCart,el)).join('')
+        console.log(cartHtml);
+        
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cartHtml)
+        res.end(output)
     }
 
     //product
     else if (pathName === '/product') {
-        res.end(tempProduct)
+
     }
     //API
     else if (pathName === '/api') {
